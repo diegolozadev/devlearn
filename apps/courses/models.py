05b46a40d6f357d2908cd1fad360229733ac6b5e
Diego <diegolozanodev@gmail.com>
2026-01-17 09:07:27 -0500

    Add new folder models and add new models

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from .models.category import Category
+from .models.course import Course, CourseCategory
+from .models.module import Module
+
 
 # Create your models here.
