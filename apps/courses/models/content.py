fb515cdb3d720487101b9fe6266d8c917290873c
Diego <diegolozanodev@gmail.com>
2026-01-26 23:41:13 -0500

    Add content models and fields for courses, modules, and related entities

--- apps/courses/models/content.py
+++ apps/courses/models/content.py
@@ -2,6 +2,35 @@ from .module import Module
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
+from ..files import OrderField
+
+# Utilizamos el modelo polimórfico -> Herencia Abstracta la cual creamos un modelo, pero este modelo no se crea en la base de datos, lo que hace es que se hereda a otros modelos que si se van a guardar en la base de datos, como lo son los modelos Text, File, Image y Video
+class ItemBase(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
+
+
 
 class Content(models.Model):
 
@@ -10,9 +39,13 @@ class Content(models.Model):
         related_name='contents',
         on_delete=models.CASCADE
     )
-    Content_type = models.ForeignKey(
+    content_type = models.ForeignKey(
         ContentType,
         on_delete=models.CASCADE,
+        limit_choices_to={
+            'model__in': ('text', 'video', 'image', 'file')
+        }
     )
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
